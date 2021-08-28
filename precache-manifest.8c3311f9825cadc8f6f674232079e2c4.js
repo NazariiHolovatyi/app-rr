50f424f8efdb047658b8f63a084c9a0da4926a66
NazariiHolovatyi <holovatyi.nazarii@gmail.com>
2021-08-28 17:37:52 +0300

    Updates

---
+++ precache-manifest.8c3311f9825cadc8f6f674232079e2c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5adf16b6e328338f64bf",
+    "url": "/app-rr/static/css/main.4c78bff0.chunk.css"
+  },
+  {
+    "revision": "5adf16b6e328338f64bf",
+    "url": "/app-rr/static/js/main.498bcab9.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "ba19c38cc61f036046ed",
+    "url": "/app-rr/static/js/2.a5c30777.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "37c525059628311ec14f0bb90948723a",
+    "url": "/app-rr/index.html"
+  }
+];
